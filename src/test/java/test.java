3755fae6d95533bd6b34e7ea9449ca70a02a7015
habioye <78454514+habioye@users.noreply.github.com>
2024-06-09 00:00:01 -0400

    Tested the project

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,67 +1,77 @@
-import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import junit.*;
 
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class test {
 
+
+
+
+
     @Test
-    public void testCRUD() {
-        String name = "box";
-        int count = 10;
-        boolean succes = false;
-
-        // Create
-        succes = InventoryManagementApp.createItem(name,count);
-        assertEquals(succes,true);
-        String output;
-
-        // Read
-        output = InventoryManagementApp.readItem(name);
-
-        assertEquals(output, "box 10");
-
-        // Update
-        succes = updateItem(name,20);
-        assertEquals(true);
-
-        // Delete
-
-
-
-
+    public void test_valid_username_and_password_returns_true() {
+        InventoryManagementApp app = new InventoryManagementApp();
+        boolean result = app.userpassExists("validUser", "validPass");
+        assertTrue(result);
     }
 
     @Test
-    public void testRead() {
-
+    public void test_username_or_password_contains_special_characters() {
+        InventoryManagementApp app = new InventoryManagementApp();
+        boolean result = app.userpassExists("user!@#", "pass$%^");
+        assertFalse(result);
     }
-    @Test
-    public void testUpdate() {
 
-    }
-    @Test
-    public void testDelete() {
 
-    }
+
 
     @Test
-    public void ensureIntegrity() {
+    public void test_prints_main_menu_options_correctly() {
+        InventoryManagementApp app = new InventoryManagementApp();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
 
+        app.displayMainMenu();
+
+        String expectedOutput = "Select from the following options for the inventory.\n" +
+                "1. Login\n" +
+                "2. Register\n" +
+                "3. View Instructions\n" +
+                "4. Exit\n";
+        assertEquals(expectedOutput, outContent.toString());
     }
 
-    @Test
-    public void searchProduct() {
 
-    }
 
-    @Test
-    public void testAuthentication() {
 
-    }
 
-    @Test
-    public void testReport() {
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
